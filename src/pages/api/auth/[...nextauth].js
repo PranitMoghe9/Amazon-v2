30e4e276ae058eb3b2339ecd7138c28af24c2ab2
PranitMoghe9 <pranitmoghe9@gmail.com>
2023-01-01 19:06:36 +0530

    app build

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,10 +7,10 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      secret: process.env.SECRET,
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 };
 
 export default NextAuth(authOptions);
