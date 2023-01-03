7e59fe76fb4980ea65fd2eeca4dc379668f7dea3
PranitMoghe9 <pranitmoghe9@gmail.com>
2023-01-03 19:09:00 +0530

    app build

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -47,7 +47,7 @@ function CheckoutProduct({
             ))}
         </div>
         <p className="text-xs my-2 line-clamp-3">{description}</p>
-        <Currency quantity={price} currency="GBP" />
+        <Currency quantity={price * 99.96} currency="INR" />
         {hasPrime && (
           <div className="flex items-center space-x-2">
             <img
