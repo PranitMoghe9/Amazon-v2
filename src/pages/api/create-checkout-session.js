577e6b5d7a51574c537edd830324adb4df2a0836
PranitMoghe9 <pranitmoghe9@gmail.com>
2023-01-03 19:03:19 +0530

    app build

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -6,8 +6,8 @@ export default async (req, res) => {
   const transformedItems = items.map((item) => ({
     quantity: 1,
     price_data: {
-      currency: "gbp",
-      unit_amount: item.price * 100,
+      currency: "inr",
+      unit_amount: item.price * 99 * 96,
       product_data: {
         name: item.title,
         images: [item.image],
